ff8fd39a4d7e2360cac604dbea0e4b9799c7d788
xhd <1365821818@qq.com>
2022-09-24 22:56:59 +0800

    20220924增加三个题目的解决方法

---
+++ function/src/main/java/xhd/practice/offer7.java
@@ -0,0 +1,18 @@
+package xhd.practice;
+
+import javax.swing.tree.TreeNode;
+
+/*
+* 输入某二叉树的前序遍历和中序遍历的结果，请构建该二叉树并返回其根节点。
+    假设输入的前序遍历和中序遍历的结果中都不含重复的数字。
+    *    前序遍历、中序遍历：根左右、左根右
+    * 例如：
+    *   Input: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+    *   Output: [3,9,20,null,null,15,7]
+* */
+public class offer7 {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+
+         return null;
+    }
+}
