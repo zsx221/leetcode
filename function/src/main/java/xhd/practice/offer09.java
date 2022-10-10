b4d91d10752b69b435375f02fdf41eee5ee7d969
xhd <1365821818@qq.com>
2022-10-10 22:44:37 +0800

    2022/10/10，解决几个简单算法题

---
+++ function/src/main/java/xhd/practice/offer09.java
@@ -0,0 +1,15 @@
+package xhd.practice;
+
+public class offer09 {
+    public void CQueue() {
+
+    }
+
+    public void appendTail(int value) {
+
+    }
+
+    public int deleteHead() {
+            return 0;
+    }
+}
--- function/src/main/java/xhd/practice/offer7.java
+++ function/src/main/java/xhd/practice/offer7.java
@@ -1,6 +1,7 @@
 package xhd.practice;
 
-import javax.swing.tree.TreeNode;
+
+import java.util.Arrays;
 
 /*
 * 输入某二叉树的前序遍历和中序遍历的结果，请构建该二叉树并返回其根节点。
@@ -12,7 +13,19 @@ import javax.swing.tree.TreeNode;
 * */
 public class offer7 {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-
-         return null;
+        int n = preorder.length;
+        if (n == 0)
+            return null;
+        int rootVal = preorder[0], rootIndex = 0;
+        for (int i = 0; i < n; i++) {
+            if (inorder[i] == rootVal) {
+                rootIndex = i;
+                break;
+            }
+        }
+        TreeNode root = new TreeNode(rootVal);
+        root.left = buildTree(Arrays.copyOfRange(preorder, 1, 1 + rootIndex), Arrays.copyOfRange(inorder, 0, rootIndex));
+        root.right = buildTree(Arrays.copyOfRange(preorder, 1 + rootIndex, n), Arrays.copyOfRange(inorder, rootIndex + 1, n));
+        return root;
     }
 }
