10c88c7917d86be8294c129bb005407d15483b79
xhd <1365821818@qq.com>
2022-10-16 23:00:29 +0800

    2022/10/16，复习三个算法题

--- function/src/main/java/xhd/practice/offer09.java
+++ function/src/main/java/xhd/practice/offer09.java
@@ -2,7 +2,6 @@ package xhd.practice;
 
 import java.util.Deque;
 import java.util.LinkedList;
-import java.util.Stack;
 
 /**用两个栈实现一个队列。队列的声明如下，请实现它的两个函数 appendTail 和 deleteHead
  * 分别完成在队列尾部插入整数和在队列头部删除整数的功能。
